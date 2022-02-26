70862b105daf7462023a8134f8dda9c1c7b493ed
Manjunath LD <ldmanju007@gmail.com>
2022-02-26 21:20:24 +0530

    Commit_006: Service Component 1. Added BOOT_COMPLETED Broadcast Receiver to to detect the boote action. After getting boot action to start the foreground service.

---
+++ app/src/main/java/com/example/androidcomponents/receiver/ReceiverBootComplete.kt
@@ -0,0 +1,15 @@
+package com.example.androidcomponents.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.androidcomponents.services.ForegroundService
+
+class ReceiverBootComplete : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent!!.action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+            val intentForegroundService: Intent = Intent(context, ForegroundService::class.java)
+            context!!.startForegroundService(intentForegroundService)
+        }
+    }
+}
